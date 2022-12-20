6d295a8d917c4577c1fcae5315617db2c8431298
Ajilo <ajex@capalaltd.com>
2022-12-20 13:14:27 -0800

    6 puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,9 @@
 
 void puts2(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; ++i)
+	for (int i = 0; str[i] != '\0'; i += 2)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
