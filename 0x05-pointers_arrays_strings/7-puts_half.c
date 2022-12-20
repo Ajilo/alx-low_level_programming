2cdd830133a57b1e5d88b6f0102722dc5d109b06
Ajilo <ajex@capalaltd.com>
2022-12-20 14:11:54 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,17 @@
  * Return: Nothing
  */
 
-void puts_half(char *str)
+void puts_half(char *str) 
 {
-	int l, n;
-
-	for (l = 0; str[l] != '\0'; ++l)
-		;
-
-	if (l % 2 == 0)
+	int len = strlen(str);
+	int half = len / 2;
+	if (len % 2 == 1)
 	{
-		for (n = l / 2; str[n] != '\0'; ++n)
-			_putchar(str[n]);
-	} else
-	{
-		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
-			_putchar(str[n]);
+		half = (len - 1) / 2;
 	}
-	_putchar('\n');
+	for (int i = half; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
