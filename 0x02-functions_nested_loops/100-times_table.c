00af16c7dc48214b839dc7de23dd963ed55838c9
Ajilo <ajex@capalaltd.com>
2022-12-21 03:46:31 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,9 +18,11 @@ void print_times_table(int n);
 		return;
 	}
 
-	for (int i = 0; i <= n; i++)
+	int i, j;
+
+	for (i =0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			printf("%d ", i * j);
 		}
