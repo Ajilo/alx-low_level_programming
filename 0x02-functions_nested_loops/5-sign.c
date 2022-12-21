abb453446a9a94d5d450b16b97625e12f7e7c3ed
Ajilo <ajex@capalaltd.com>
2022-12-21 03:05:49 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,27 +8,34 @@
  *
  */
 
-int print_sign(int n) {
-  if (n > 0) {
-    printf("+\n");
-    return 1;
-  } else if (n == 0) {
-    printf("0\n");
-    return 0;
-  } else {
-    printf("-\n");
-    return -1;
-  }
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+\n");
+		return 1;
+	}
+	else if (n == 0)
+	{
+		printf("0\n");
+		return 0;
+	}
+	else
+	{
+		printf("-\n");
+		return -1;
+	}
 }
 
-int main() {
+int main()
+{
   int x = -5;
   int y = 0;
   int z = 10;
 
-  print_sign(x);  // prints '-'
-  print_sign(y);  // prints '0'
-  print_sign(z);  // prints '+'
+  print_sign(x);  /* prints '-' */
+  print_sign(y);  /* prints '0' */
+  print_sign(z);  /* prints '+' */
 
   return 0;
 }
